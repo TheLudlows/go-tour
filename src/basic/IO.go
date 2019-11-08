02a50aab6d94bee47fa7c0204f550fe3c2132d5e
four <ai-l@outlook.com>
2019-11-08 18:41:22 +0800

    init

---
+++ src/basic/IO.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func readFormReader(reader io.Reader) []byte {
+	bytes := make([]byte, 100)
+	n, _ := reader.Read(bytes)
+	return bytes[:n]
+}
+func main() {
+	//bytes := readFormReader(strings.NewReader("four hello"))
+	//bytes := readFormReader(os.Stdin)
+
+	file, error := os.Open(".IO.go")
+	defer file.Close()
+	bytes := readFormReader(file)
+	fmt.Println(error)
+	fmt.Println(bytes)
+
+}
