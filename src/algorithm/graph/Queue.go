946aa9257b91264657da2f058c46808737b00ad4
four <ai-l@outlook.com>
2019-11-29 00:30:10 +0800

    update

--- src/algorithm/graph/Queue.go
+++ src/algorithm/graph/Queue.go
@@ -2,20 +2,20 @@ package graph
 
 import "sync"
 
-type NodeQueue struct {
+type Queue struct {
 	nodes []Node
 	lock  sync.RWMutex
 }
 
 // 入队列
-func (q *NodeQueue) Enqueue(n Node) {
+func (q *Queue) Enqueue(n Node) {
 	q.lock.Lock()
 	defer q.lock.Unlock()
 	q.nodes = append(q.nodes, n)
 }
 
 // 出队列
-func (q *NodeQueue) Dequeue() *Node {
+func (q *Queue) Dequeue() *Node {
 	q.lock.Lock()
 	defer q.lock.Unlock()
 	node := q.nodes[0]
@@ -24,15 +24,15 @@ func (q *NodeQueue) Dequeue() *Node {
 }
 
 // 判空
-func (q *NodeQueue) IsEmpty() bool {
+func (q *Queue) IsEmpty() bool {
 	q.lock.RLock()
 	defer q.lock.RUnlock()
 	return len(q.nodes) == 0
 }
 
 // 生成节点队列
-func NewNodeQueue() *NodeQueue {
-	q := NodeQueue{}
+func NewNodeQueue() *Queue {
+	q := Queue{}
 	q.lock.Lock()
 	defer q.lock.Unlock()
 	q.nodes = []Node{}
--- src/algorithm/graph/Stack.go
+++ src/algorithm/graph/Stack.go
@@ -4,16 +4,22 @@ type Stack struct {
 	nodes []Node
 }
 
-func (stack Stack) push(node Node) {
+func (stack *Stack) push(node Node) {
 	stack.nodes = append(stack.nodes, node)
 }
 
-func (stack Stack) pop() (node *Node) {
+func (stack *Stack) pop() *Node {
 	l := len(stack.nodes)
-	if l == 0 {
-		return node
-	}
-	node = &stack.nodes[l-1]
+	node := stack.nodes[l-1]
 	stack.nodes = stack.nodes[:l-1]
-	return node
+	return &node
+}
+
+func NewStack() Stack {
+	stack := Stack{}
+	stack.nodes = []Node{}
+	return stack
+}
+func (stack *Stack) isEmpty() bool {
+	return len(stack.nodes) == 0
 }
