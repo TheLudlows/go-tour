dc377710e0eea184da3d055151f84c644197cab9
four <ai-l@outlook.com>
2020-04-27 20:02:03 +0800

    update

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-tour
+
+go 1.14
+
+require github.com/prometheus/client_golang v1.5.1 // indirect
--- prometheus/Demo.go
+++ prometheus/Demo.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
@@ -11,7 +10,6 @@ import (
 var addr1 = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
 
 func main() {
-	fmt.Println("add", *addr1)
 	flag.Parse()
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(*addr1, nil))
